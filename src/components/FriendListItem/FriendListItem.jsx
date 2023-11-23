ae931927617e946121d3b6ababa5f551fd040da0
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-11-23 16:43:11 +0200

    update
    status, css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
 export function FriendListItem({ avatar, name, isOnline }) {
+  const backgroundColor = isOnline ? 'green' : 'red';
+
   return (
     <>
-      <span className={`${css.status}`}>{isOnline}</span>
-      <img
-        className={`${css.avatar}`}
-        src={avatar}
-        alt="User avatar"
-        style={{ width: '48px' }}
-      />
-      <p className={`${css.name}`}>{name}</p>
+      <img className={`${css.avatar}`} src={avatar} alt="User avatar" />
+      <div className={`${css.statusBox}`}>
+        <span style={{ backgroundColor }} className={`${css.status}`}>
+          {isOnline}
+        </span>
+        <p className={`${css.name}`}>{name}</p>
+      </div>
     </>
   );
 }
